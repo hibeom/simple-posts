03ee4464d66ba67b140f8c18c476b105e0054d87
hibeom <hbkim1293@gmail.com>
2022-02-04 22:19:42 +0900

    first commit

---
+++ app/src/main/java/com/pinkcloud/simpleposts/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.pinkcloud.simpleposts
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+}
---
+++ app/src/main/java/com/pinkcloud/simpleposts/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.pinkcloud.simpleposts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+    }
+}
