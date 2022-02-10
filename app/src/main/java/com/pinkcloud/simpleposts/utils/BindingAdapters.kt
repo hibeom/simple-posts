ef87632aa56b84bc0f2a5fb086314c9b30d5acfc
hibeom <hbkim1293@gmail.com>
2022-02-10 20:41:42 +0900

    nested scroll view

--- app/src/main/java/com/pinkcloud/simpleposts/utils/BindingAdapters.kt
+++ app/src/main/java/com/pinkcloud/simpleposts/utils/BindingAdapters.kt
@@ -2,8 +2,14 @@ package com.pinkcloud.simpleposts.utils
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.pinkcloud.simpleposts.R
 
 @BindingAdapter("name", "email")
 fun setCommentTitle(textView: TextView, name: String, email: String) {
-    textView.text = "$name \u2022 $email"
+
+    textView.text = String.format(
+        textView.resources.getString(R.string.comment_user),
+        name,
+        email
+    )
 }
